3423f0b494e6af222ad2153d06f47968ec00f7ad
Suarez_Ken <kensuarez31@gmail.com>
2025-08-14 13:01:24 +0800

    Merge ColorChangerApp and CounterApp into master

--- App.js
+++ App.js
@@ -1,3 +1,59 @@
+import React from 'react';
+import { SafeAreaView, ScrollView, View } from 'react-native';
+
+import ChatScreen from './ChatScreen';
+import Comment from './Comment';
+import CounterApp from './CounterApp';
+import ColorChangerApp from './ColorChangerApp';
+
+export default function App() {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <ScrollView contentContainerStyle={{ flexGrow: 1, padding: 16 }}>
+        
+        <View style={{ marginBottom: 20 }}>
+          <Comment />
+        </View>
+
+        <View style={{ marginBottom: 20 }}>
+          <ChatScreen />
+        </View>
+
+        <View style={{ marginBottom: 20 }}>
+          <CounterApp />
+        </View>
+
+        <View style={{ marginBottom: 20 }}>
+          <ColorChangerApp />
+        </View>
+import React from "react";
+import { SafeAreaView, ScrollView, View, StyleSheet } from "react-native";
+import ChatScreen from "./ChatScreen";
+import Comment from "./Comment";
+import ColorChangerApp from "./ColorChangerApp";
+
+export default function App() {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <ScrollView contentContainerStyle={styles.container}>
+        {/* Chat + Comments */}
+        <Comment />
+        <ChatScreen />
+
+        {/* Color Changer */}
+        <ColorChangerApp />
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    padding: 10,
+  },
+});
+<<<<<<< HEAD
 import React from "react";
 import ChatScreen from "./ChatScreen";
 import Comment from "./Comment";
@@ -11,3 +67,28 @@ export default function App() {
     </SafeAreaView>
     );
 }
+=======
+import React from 'react';
+import { View, StyleSheet, ScrollView } from 'react-native';
+
+import ColorChangerApp from './ColorChangerApp';
+
+const App = () => {
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <ColorChangerApp />
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    paddingVertical: 20,
+  },
+});
+
+export default App;
+>>>>>>> 08071db (ColorChangerApp)
