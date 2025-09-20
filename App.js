d8a89ede87435f707d98688e2fadfeb35833fb7a
Suarez_Ken <kensuarez31@gmail.com>
2025-09-20 20:49:42 +0800

    Update App.js in Activity5 branch

--- App.js
+++ App.js
@@ -1,4 +1,17 @@
+import ColorChangerApp from './ColorChangerApp';
 import React from 'react';
+import { SafeAreaView } from 'react-native';
+import CounterApp from './CounterApp';  
+
+export default function App() {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+     <CounterApp />
+      <ColorChangerApp />
+ 
+    </SafeAreaView>
+  );
+}import React from 'react';
 import { View, StyleSheet, ScrollView } from 'react-native';
 
 import ColorChangerApp from './ColorChangerApp';
