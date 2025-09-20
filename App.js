06bbfd36ee0921665fe96ec6fe3b6c2ca00b1cda
Suarez_Ken <kensuarez31@gmail.com>
2025-09-20 20:57:54 +0800

    Fix App.js code in Master

--- App.js
+++ App.js
@@ -26,31 +26,9 @@ export default function App() {
         <View style={{ marginBottom: 20 }}>
           <ColorChangerApp />
         </View>
-import React from "react";
-import { SafeAreaView, ScrollView, View, StyleSheet } from "react-native";
-import ChatScreen from "./ChatScreen";
-import Comment from "./Comment";
-import ColorChangerApp from "./ColorChangerApp";
 
-export default function App() {
-  return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <ScrollView contentContainerStyle={styles.container}>
-        {/* Chat + Comments */}
-        <Comment />
-        <ChatScreen />
-
-        {/* Color Changer */}
-        <ColorChangerApp />
       </ScrollView>
     </SafeAreaView>
   );
 }
-
-conststyles = StyleSheet.create({
-  container: {
-    flexGrow: 1,
-    padding: 10,
-  },
-});
 
