76d375d4c5b514a03c3e340f6252947f3e220e38
Suarez_Ken <kensuarez31@gmail.com>
2025-09-20 21:01:41 +0800

    Tsrung code sa App.js Act5

--- App.js
+++ App.js
@@ -11,26 +11,4 @@ export default function App() {
  
     </SafeAreaView>
   );
-}import React from 'react';
-import { View, StyleSheet, ScrollView } from 'react-native';
-
-import ColorChangerApp from './ColorChangerApp';
-
-const App = () => {
-  return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <ColorChangerApp />
-    </ScrollView>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flexGrow: 1,
-    alignItems: 'center',
-    justifyContent: 'space-around',
-    paddingVertical: 20,
-  },
-});
-
-export default App;
+}
